81e7b00e4c9c0cbc728be162145e86e565ee751e
Alan Budiarto <alanwbudiarto@gmail.com>
2020-04-22 00:48:47 +0700

    Added Buefy

--- webclient/src/main.js
+++ webclient/src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+import '@/assets/style.css'
+ 
+Vue.use(Buefy);
 Vue.config.productionTip = false
 
 new Vue({
