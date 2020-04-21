eb3edf486a015cf4eafd6c669d64f08041e1c483
Alan Budiarto <alanwbudiarto@gmail.com>
2020-04-22 00:49:12 +0700

    Added Start Button

--- webclient/src/App.vue
+++ webclient/src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  // @font-face { 
+  //   font-family: "Montserrat-Light";
+  //   src: url("assets/Montserrat-Light.ttf"); 
+  // }
+  // @font-face { 
+  //   font-family: "Montserrat-Medium";
+  //   src: url("assets/Montserrat-Medium.ttf"); 
+  // }
+  // @font-face { 
+  //   font-family: "Montserrat-Regular";
+  //   src: url("assets/Montserrat-Regular.ttf"); 
+  // }
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 300;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background: #FFFAFA;
 }
 </style>
